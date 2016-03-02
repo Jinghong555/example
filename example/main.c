02a9a468aefe50d0cd0300a7f946d7a0ba29155d
邵景洪 <shaojinghong@shaojinghongdemacbook-pro.local>
2016-03-02 19:17:08 +0800

    Initial Commit

---
+++ example/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  example
+//
+//  Created by 邵景洪 on 16/3/2.
+//  Copyright © 2016年 c language. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
